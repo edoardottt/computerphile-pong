c2f6d4b11dc1b958a619207bf0fa50fc340361eb
edoardottt <edoardott@gmail.com>
2020-06-17 22:26:16 +0200

    squashpong

--- game.py
+++ game.py
@@ -10,7 +10,19 @@ import pygame
 
 
 #=============PREPARATION============
+
+squashpong = '''
+                           _                             
+ ___  __ _ _   _  __ _ ___| |__  _ __   ___  _ __   __ _ 
+/ __|/ _` | | | |/ _` / __| '_ \| '_ \ / _ \| '_ \ / _` |
+\__ \ (_| | |_| | (_| \__ \ | | | |_) | (_) | | | | (_| |
+|___/\__, |\__,_|\__,_|___/_| |_| .__/ \___/|_| |_|\__, |
+        |_|                     |_|                |___/ 
+'''
 pygame.init()
+print("https://github.com/edoardottt/squashpong")
+print("https://edoardoottavianelli.it")
+print(squashpong)
 
 WIDTH = 1200
 HEIGHT = 600
